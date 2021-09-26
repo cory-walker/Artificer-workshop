84e118bbc327e11ad32196c4d6be5e7db87dbbbb
cory walker <cory-walker@live.com>
2021-09-26 14:26:29 -0400

    Added spalsh screen

--- eldrictworkshop.py
+++ eldrictworkshop.py
@@ -7,6 +7,28 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.core.window import Window
+
+cur_item = None
+
+
+class magicItem:
+    def __init__(self, name="new item", description="", eldrict_properties=[], rarity="common", is_artifact=False, is_consumable=False, eldrict_materials=[]):
+        self.name = name
+        self.description = description
+        self.eldrict_properties = eldrict_properties
+        self.rarity = rarity
+        self.is_artifact = is_artifact
+        self.is_consumable = is_consumable
+
+        self.eldrict_materials = eldrict_materials
+
+    def get_properties(self):
+        l = self.eldrict_properties
+        for material in self.eldrict_materials():
+            if material.eldrict_property is not None:
+                l.append(material.eldrict_property)
+        return l
 
 
 class Display(BoxLayout):
@@ -24,15 +46,17 @@ class LoadItemScreen(Screen):
         super(LoadItemScreen, self).__init__(**kwargs)
         self.load_item_list = self.build_load_items_list()
 
-        dd = DropDown(size_hint_y=None, size_hint_x=None)
+        dd = DropDown(size_hint_y=1, size_hint_x=None, width=200)
         self.add_widget(dd)
         for il in self.load_item_list:
-            btn = Button(text=il, size_hint=(None, None))
+            btn = Button(text=il, size_hint_y=None,
+                         size_hint_x=None, height=30)
             btn.bind(on_release=self.print_something)
             dd.add_widget(btn)
 
     def print_something(self, *args):
         print(args[0].text)
+        self.load_item(args[0].text)
         self.parent.current = "crafting"
 
     def build_load_items_list(self):
@@ -47,6 +71,37 @@ class LoadItemScreen(Screen):
             lst.append(l[0])
         f.close()
         return lst
+
+    def load_item(self, item_name):
+        scrpt_dir = path.dirname(__file__)
+        filepath = path.join(scrpt_dir, ".\\data\\magic_items.csv")
+        f = open(filepath, "r")
+        reader = csv.reader(f, delimiter=',', quotechar='"',
+                            skipinitialspace=True)
+        next(reader, None)  # skip headers
+        for l in reader:
+            if l[0] == item_name:
+                break
+
+        cur_item = magicItem(name=l[0], description=l[1], rarity=l[2],
+                             is_artifact=l[3], is_consumable=l[4])
+        spl = l[5].split(",")
+        for s in spl:
+            cur_item.eldrict_properties.append(
+                s.replace("[", "").replace("]", ""))
+
+        spl = l[6].split(",")
+        for s in spl:
+            cur_item.eldrict_materials.append(
+                s.replace("[", "").replace("]", ""))
+
+        f.close()
+        return
+
+
+class SplashScreen(Screen):
+    def __init__(self, **kwargs):
+        super(SplashScreen, self).__init__(**kwargs)
 
 
 class CraftingScreen(Screen):
